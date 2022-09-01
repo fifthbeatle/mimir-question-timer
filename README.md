248416e7546fcef0d6259ee55493952ac3f27e1d
Arnold C Dsouza <arnold.dsouza@in.ibm.com>
2022-09-01 07:44:38 +0530

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mimir-question-timer
-Add a timer onto the Wikiquiz scoring app and make the passing order sticky 
+
+## Description
+Chrome extension to add a timer onto the Wikiquiz scoring page and make passing order sticky
+
+## Instructions for installation
+1. Download the zip file `mimir-question-timer.7z`.
+2. Extract it to a folder `mimir-question-timer`.
+3. Open your Extensions page in Chrome either by going to `chrome://extensions` or by selection "More tools" -> "Extensions" from the Chrome menu.
+4. Switch on "Developer Mode" in the top right corner.
+5. Click on "Load Unpacked" and select the folder `mimir-question-timer` that you extracted.
+
+## Instructions for use
+1. Add the timer onto the page by clicking on the extension icon in your browser **after** starting your game.
+2. Start the timer after you finish reading the question by clicking on it.
+3. The timer for passed questions will start automatically after clicking on "Wrong"/"Pass".
+
+## Contact
+1. Email me at **dsouza.arnold@gmail.com** for any clarifications or to report any bugs.
+2. Updated versions of the app will be uploaded to this same repository.
---
+++ code/content.js
@@ -0,0 +1,128 @@
+let spans = document.getElementsByTagName("span")
+let currentTimeValue = 0
+let startTimeValue = 0
+let passTimeValue = 0
+const warningThreshold = 10
+let epochEndTime = null
+let interval = 100
+let timer = null
+let newQuestion = null
+
+let mutationObserver = new MutationObserver(() => {
+  clearInterval(timer)
+  currentTimeValue = startTimeValue
+  $("#timer-button").attr({ value: startTimeValue })
+  $("#timer-button").css({ backgroundColor: "lightblue" })
+  newQuestion = true
+})
+
+const spanStyles = { display: "inline-block", float: "right" }
+const buttonStyles = {
+  border: "none", borderRadius: "5px", padding: "15px 40px",
+  backgroundColor: "lightblue", fontFamily: "Menlo, Consolas, monospace"
+}
+
+const getTimeRemaining = () => {
+  const now = new Date()
+  return Math.ceil((epochEndTime - now) / 1000)
+}
+
+const startTimer = () => {
+  newQuestion = false
+  currentTimeValue = getTimeRemaining()
+  $("#timer-button").attr({ value: Math.ceil(currentTimeValue) })
+  if (currentTimeValue <= warningThreshold) {
+    $("#timer-button").css({ backgroundColor: "lightpink" })
+  } else {
+    $("#timer-button").css({ backgroundColor: "lightblue" })
+
+  }
+  if (currentTimeValue <= 0) {
+    clearInterval(timer)
+  }
+}
+
+const createNewButton = (name, value) => {
+  const $target = $(".tablecell3.ng-binding")[0]
+  const $mySpan = $("<span/>").css(spanStyles)
+  const $myButton = $('<input>').attr({ id: "timer-button", value: startTimeValue, type: "button" }).css(buttonStyles)
+  currentTimeValue = startTimeValue
+
+  $myButton.click(() => {
+    clearInterval(timer)
+    epochEndTime = new Date(new Date().getTime() + startTimeValue * 1000)
+    currentTimeValue = startTimeValue
+
+    timer = setInterval(startTimer, interval)
+  })
+  $mySpan.append($myButton[0])
+  $target.append($mySpan[0])
+
+  //Add an observer to watch for changes to the Question Number
+  mutationObserver.observe(document.getElementsByClassName("tablecell3 ng-binding")[0],
+    { characterData: true, subtree: true, childList: true }
+  )
+}
+
+const removeClockStickiness = () => {
+  const $clock = $(".cell.w24.h7.ng-scope")
+  $clock.css({ position: "relative" })
+}
+
+const makePassingOrderSticky = () => {
+  const $dashboard = $(".cell.w99.h16:first")
+  $dashboard.css({ position: "sticky", top: "0", height: "90px" })
+}
+
+function correctButtonHandler() {
+  clearInterval(timer)
+  currentTimeValue = startTimeValue
+  $("#timer-button").attr({ value: startTimeValue })
+  $("#timer-button").css({ backgroundColor: "lightblue" })
+  removeClockStickiness()
+}
+
+function passWrongButtonHandler() {
+  if (!newQuestion) {
+    clearInterval(timer)
+    currentTimeValue = passTimeValue
+    $("#timer-button").attr({ value: passTimeValue })
+    $("#timer-button").css({ backgroundColor: "lightpink" })
+    epochEndTime = new Date(new Date().getTime() + passTimeValue * 1000)
+    timer = setInterval(startTimer, interval)
+  }
+  removeClockStickiness()
+}
+
+const setButtonActions = () => {
+  const correctButton = document.querySelectorAll("input[type=button]")[8]
+  const passButton = document.querySelectorAll("input[type=button]")[9]
+  const wrongButton = document.querySelectorAll("input[type=button]")[10]
+  const killButton = document.querySelectorAll("input[type=button]")[11]
+
+  correctButton.addEventListener("click", correctButtonHandler)
+  passButton.addEventListener("click", passWrongButtonHandler)
+  wrongButton.addEventListener("click", passWrongButtonHandler)
+  killButton.addEventListener("click", correctButtonHandler)
+}
+
+chrome.runtime.onMessage.addListener((request, _sender, _response) => {
+  startTimeValue = request.startTimeValue
+  passTimeValue = request.passTimeValue
+
+  if ($("#timer-button").length == 0) {
+    createNewButton("Start Timer")
+  } else {
+    console.log("Button already exists.")
+  }
+  removeClockStickiness()
+  makePassingOrderSticky()
+  setButtonActions()
+
+  /*[TODO] this is a quick-and-dirty hack to keep the timer "unsticky".
+  It is currently becoming sticky randomly during the game due to some unknown event. Need to investigate what.
+  */
+  document.addEventListener("scroll", _event => {
+    removeClockStickiness()
+  })
+})
